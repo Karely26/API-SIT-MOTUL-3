147877fee9eb93de45aa83c5b705c8b74a1ed404
«karely26» <cristhian.20070032@itsmotul.edu.mx>
2023-12-03 00:14:40 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Api from './components/Api.vue';
+import Eval from './components/Eval.vue';
+import EvalIng from './components/EvalIng.vue';
+import Datos from './components/Datos.vue';
+</script>
+
+<template>
+  <div>
+    <header>
+      <Api></Api>
+    </header>
+    <body>
+      <Eval></Eval>
+      <div class="espacio"></div>
+      <Datos></Datos>
+      <div class="espacio"></div>
+      <EvalIng></EvalIng>
+      <div class="espacio"></div>
+    </body>
+  </div>
+</template>
+
+<style>
+  /* Estilo para agregar espacio hacia abajo */
+  .espacio {
+    background-color: rgb(229, 152, 102); /* Cambia el color según tus preferencias */
+    height: 40px; /* Ajusta el valor según tus necesidades */
+  }
+</style>
+
---
+++ src/components/Api.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+
+const info = ref('');
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://sitmotul.com.mx/api/statusEval')
+        const data = await response.json();
+        info.value = data
+        console.log(info.value);
+    } catch (error) {
+        console.error('Error al obtener datos:', error);
+    }
+}
+onMounted(() => {
+    fetchData();
+})
+</script>
+
+<template>
+  <body class="fond">
+    <header class="w-full">
+      <div class="w-full flex flex-col gap-y-3">
+        <div class="w-full flex flex-col gap-y-1 place-items-center px-4  md:px-8 lg:px-16">
+          <img alt="Sit logo" class="logo" src="@/components/icons/logoS.svg" width="200" height="200" />
+          <h1 class="title">SITMOTUL</h1>
+          <h3 class="text">Información sobre la evaluación al tutor del ciclo escolar {{ info.periodo }}</h3>
+        </div>
+      </div>
+    </header>
+  </body>
+  
+  
+</template>
+<style scoped>
+    /* Estilos generales que se aplicarán en todas las pantallas */
+    .fond {
+      background-color: rgb(175, 96, 26);
+    }
+
+    .title {
+      font-size: 25px;
+    }
+
+    .text {
+      font-size: 20px;
+    }
+
+    .bolded {
+      font-weight: bold; 
+      font-size: 20px;
+      text-decoration: underline;
+    }
+
+    .bolded2 {
+      font-weight: bold; 
+      font-size: 20px;
+    }
+
+    header {
+      line-height: 1.5;
+      max-height: 100vh;
+    }
+
+    .logo {
+      display: block;
+      margin: 0 auto 2rem;
+    }
+
+    @media only screen and (max-width: 600px) {
+      /* Estilos específicos para pantallas más pequeñas */
+      .fond {
+        background-color: rgb(175, 96, 26);
+      }
+
+      /* Otros estilos para móviles */
+    }
+  </style>
